140bfe36b0bd1ff99ad72aaa5c8a435e99e4fe45
Abhijeet Singh Parihar <anshhhh137@gmail.com>
2025-01-03 01:21:38 +0530

    Delete src directory

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-// models/index.ts
-import {User} from './User';
-import {Wallet} from './Wallet';
-import {GameSettings} from './GameSettings';
-import {GameData} from './GameData';
-import { Ad } from './Ad';
-export { User, Wallet, GameSettings, GameData,Ad };
